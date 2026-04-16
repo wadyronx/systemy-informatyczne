febb119e07df6a24b20d9781fa44b8e5d829ede9
Jakub Kiermaszek <kuba.kiermaszek@op.pl>
2026-04-16 14:16:49 +0200

    Code for second branch

---
+++ JakisProjekt/Kod_do_drugiego_branchacs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JakisProjekt
+{
+    public class Kod_do_drugiego_branchacs
+    {
+        private double a;
+        private double b;
+        private double wynik;
+
+        public double Mnozenie(double a, double b)
+        {
+            double wynik = a * b;
+            return wynik;
+        }
+    }
+}
